db86b41f972a84b6bb4e709dbe56061d098ac00e
dramane188 <72475690+dramane188@users.noreply.github.com>
2021-01-05 22:14:51 -0500

    added

--- data/api_keys.py
+++ data/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
+#weather_api_key = "2f430d7e718eebfb75723945b3a94d3d"
+weather_api_key = "5c60f22f96429fbb2dc032a911b30f74"
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBpV7rCt_Gf3KUQyB-6EjgrMST9QN90hCw"
+#API Key: "3b5c47609f814b5e94a131747210201"
